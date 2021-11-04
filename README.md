923aeb42c6746a5eff7ae333eadc8865b63c4179
JohnWes7 <1246215651@qq.com>
2021-11-05 02:16:15 +0800

    0.553

--- README.md
+++ README.md
@@ -1,19 +1,37 @@
 # Daily_Nutrition
-###  最好使用谷歌浏览器chrome  火狐浏览器没测 当作不支持  
+###  使用 Edge 或者 谷歌浏览器chrome  火狐浏览器没测 当作不支持  
 &nbsp;
-# 食用方法
-## 运行 downloads.py下载pixiv推荐的插画 可在Config.ini 编辑参数  
-## （像是抓取多少张 pixiv发现模式 r18|all|save） 
-## 第一次运行会需要登录 登录后会自动保存cookie 在cookie有效期内不需要再次登录  
-
-&nbsp;
-## 运行 discovery（还在做）可直接跳转到pixiv发现页面 程序会在浏览器关闭的时候自动下载所有用户点过爱心的项目  
-## *手动跳转到pixiv主页面 浏览器自动安全关闭 直接点X退出浏览器会报错
-
+># 食用方法
+>>## 前提条件  
+>>### 1. python 环境 （current version Python 3.9.7）
+>>### 2. selenium (pip install selenium)
+>>### 3. lxml (pip install lxml)
+>>### 4. 一个魔法上网的梯子 (自己用的是ssr **没有梯子能稳定连接github?** [梯子广告招租位](https://github.com/JohnWes7/Daily_Nutrition) Config.ini 可以设置是否启用代理以及代理网址和端口 默认是ssr的默认127.0.0.1:1080)
+>>### 5. Edge(当前: 95.0.1020.40)|Chrome(当前: 95.0.4638.69)|~~FireFox~~
+>>### 如果版本不兼容可以去找对应版本的driver替换文件夹里的同名文件即可 [Edge的driver 点这对照自己的版本下载](https://developer.microsoft.com/zh-cn/microsoft-edge/tools/webdriver/)
+>&nbsp;
+>>## 1. 全自动发现下载器 运行downloads文件
+>>### 运行 downloads.py下载pixiv推荐的插画 可在Config.ini 编辑参数（像是抓取多少张 pixiv发现模式 r18|all|save 等） 
+>>### 第一次运行会需要登录 
+>>### 登录后会自动保存cookie(在cookie有效期内不需要再次登录)  
+>>### 登陆完成后当浏览器跳转至pixiv主页时会自动关闭并记录cookie (path='./data/cookies.json')
+>>### 获取推荐和下载会在浏览器自动关闭后由控制台提示获取到的所有id 确认后回车下载
+>>### 成功下载过的id会被保存进 ./data/downloadrecord.json 设置可以选择是否用此文件排除重复id
+>>### ***不说了 mode = r18 开冲！***
+>&nbsp;
+>>## 2. 冲-收藏-下载-器 运行 discovery文件
+>>### 相当于本次进行了收藏的项目
+>>### 运行 discovery 可直接跳转到 [pixiv发现](https://www.pixiv.net/discovery) 页面 如果本地有cookie可以不用再登录自动跳转到[pixiv发现](https://www.pixiv.net/discovery)
+>>### 只要没有回到主页面(因为会导致我觉得你好了然后自动关闭) 把涩图都点个爱心加个收藏 ***放开了冲***
+>>### ***冲完了*** 程序会在浏览器关闭的时候自动下载所有 **本次** 用户点过爱心的项目 
+>>### *手动跳转到[pixiv主页面](https://www.pixiv.net/) (可以浏览器拉到最上面按左上角的PIXIV图标进行跳转) 届时浏览器则会自动安全关闭  直接点X退出浏览器**是可以的** 但需要等一小会才会自动下
+>>### ***我好了 你呢？***
 <br/>
-chrome和edge executable_path说是被弃用(但好像测的还能用) 为了保险还是扔出来  
 <br/>
-edge驱动和firefox驱动在driver文件夹内  
+chrome和edge executable_path说是被弃用(但好像测的还能用)  
+所以这两个的driver就在第一级的文件夹内 和download还有discovery同级
+<br/>
+firefox驱动在driver文件夹内  
 <br/>
 如有打开没效果 请按照自己的浏览器版本更换driver  
 <br/>
--- discovery.py
+++ discovery.py
@@ -41,13 +41,13 @@ def analysis_log(log: str, post_data_list: list):
 
         if requ.get('url') == None:
             continue
-        
-        #加入收藏时触发
+
+        # 加入收藏时触发
         if requ.get('url').__eq__(add_book_url) and requ.get('method').__eq__("POST"):
             print(requ.get('postData'))
             post_data_list.append(json.loads(requ.get('postData')))
-        
-        #取消收藏时触发
+
+        # 取消收藏时触发
         if requ.get('url').__eq__(delete_book_url) and requ.get('method').__eq__("POST"):
             print(requ.get('postData'))
             try:
@@ -59,15 +59,14 @@ def analysis_log(log: str, post_data_list: list):
                 post_data_list.append(qs_dict)
             except Exception as e:
                 print(e)
-        
-        match = re.match(f'{bookmark_event_url}.*',requ.get('url'))
+
+        match = re.match(f'{bookmark_event_url}.*', requ.get('url'))
         if not match == None:
-            tempdict = {'url':requ.get('url')}
+            tempdict = {'url': requ.get('url')}
             post_data_list.append(tempdict)
 
-
-    print(f'analysis: loglen:{log.__len__()}\tparams_none:{params_none}\trequ_none:{requ_none}\tpost_list:{post_list.__len__()}')
-
+    print(
+        f'analysis: loglen:{log.__len__()}\tparams_none:{params_none}\trequ_none:{requ_none}\tpost_list:{post_list.__len__()}')
 
 
 def delegate_title_is_pixiv(x: WebDriver):
@@ -77,7 +76,7 @@ def delegate_title_is_pixiv(x: WebDriver):
     '''
     log = []
     log = x.get_log('performance')
-    analysis_log(log,post_list)
+    analysis_log(log, post_list)
 
     title = x.title
     return 'pixiv'.__eq__(title)
@@ -85,8 +84,10 @@ def delegate_title_is_pixiv(x: WebDriver):
 
 def open_discovery():
 
-    od_dict = custom_driver.get_custom_options_desired_capabilities(config.get_browser(), config.get_is_proxies, True)
-    driver = custom_driver.get_custom_driver(name=config.get_browser(),options=od_dict.get('options'),desired_capabilities=od_dict.get('desired_capabilities'))
+    od_dict = custom_driver.get_custom_options_desired_capabilities(
+        config.get_browser(), config.get_is_proxies, True)
+    driver = custom_driver.get_custom_driver(name=config.get_browser(), options=od_dict.get(
+        'options'), desired_capabilities=od_dict.get('desired_capabilities'))
     # driver.maximize_window() # 全屏展开
 
     driver.get(config.pixiv)  # 打开网页
@@ -102,7 +103,8 @@ def open_discovery():
     # 刷新
     driver.refresh()
     # 等待到主页面
-    WebDriverWait(driver=driver, timeout=99999,poll_frequency=1).until(delegate_title_is_pixiv)
+    WebDriverWait(driver=driver, timeout=99999,
+                  poll_frequency=1).until(delegate_title_is_pixiv)
     cookies = driver.get_cookies()
     downloads.update_local_cookies(cookies)
 
@@ -110,10 +112,11 @@ def open_discovery():
     try:
         driver.get(config.discover_page)
         # 等待从发现页面出来然后关闭
-        WebDriverWait(driver=driver, timeout=99999,poll_frequency=1).until(delegate_title_is_pixiv)
+        WebDriverWait(driver=driver, timeout=99999,
+                      poll_frequency=1).until(delegate_title_is_pixiv)
     except Exception as e:
         print(e)
-    
+
     # log = driver.get_log('performance')
     # for item in log:
     #     message = json.loads(item.get('message'))
@@ -124,11 +127,13 @@ def open_discovery():
 
     driver.quit()
 
+
 def test():
     bookmark_event_url2 = 'https://event.pixiv-recommend.net/?platform=pc&action=click-bookmark'
     sample1 = "https://event.pixiv-recommend.net/?platform=pc&action=click-bookmark&zone=discovery&method=clustering_bqalgc&illust_id=92834061&seed_illust_ids=90553117%2C93470454%2C93623887%2C93790321&login=yes&user_id=25832134&p_ab_id=2&p_ab_id_2=8&p_ab_d_id=773787977"
     sample2 = 'https://event.pixiv-recommend.net/?platform=pc&action=click-bookmark&zone=discovery&method=clustering_bqalgc&illust_id=91975962&seed_illust_ids=86075831%2C91123048%2C91766083%2C93623887%2C93686296%2C93790321&login=yes&user_id=25832134&p_ab_id=0&p_ab_id_2=2&p_ab_d_id=320056489'
-    match = re.match('https://event.pixiv-recommend.net/\?platform=pc&action=click-bookmark.*',sample1)
+    match = re.match(
+        'https://event.pixiv-recommend.net/\?platform=pc&action=click-bookmark.*', sample1)
     print(match.group())
 
 
@@ -138,19 +143,20 @@ def get_pid_list():
         id = item.get('illust_id')
         if not id == None:
             d_list.append(id)
-    
+
     return d_list
 
 
 if __name__ == '__main__':
-   
+
     try:
         open_discovery()
     except Exception as e:
         print(e)
-    downloads.save_str_data(config.bookmarkdata_path,json.dumps(post_list))
+    downloads.save_str_data(config.bookmarkdata_path, json.dumps(post_list))
 
     d_list = get_pid_list()
-    print(f'开始执行下载\n将要执行下载：{len(d_list)}\n',d_list)
-    downloads.download_idlist(id_list=d_list,head=downloads.get_head_with_cookie())
-    input('done')
+    print(f'开始执行下载\n将要执行下载：{len(d_list)}\n', d_list)
+    downloads.download_idlist(
+        id_list=d_list, head=downloads.get_head_with_cookie())
+    input('='*30,'done','='*30)
