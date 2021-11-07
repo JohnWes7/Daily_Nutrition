4e1b45ee324072ccf352c5a9f448834cc13645b9
JohnWes7 <1246215651@qq.com>
2021-11-07 20:41:03 +0800

    0.556

--- check.py
+++ check.py
@@ -1,4 +1,10 @@
 from src import tool
 
+module_dict = {
+    'lxml' : 'lxml',
+    'selenium' : 'selenium'
+}
+
+
 if __name__ == '__main__':
-    tool.check_module()
+    tool.check_module(module_dict)
