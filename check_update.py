28a77e21a8df71f5cae826ad13bd104ea4b5b253
JohnWes7 <1246215651@qq.com>
2021-11-08 23:14:15 +0800

    Update check_update.py

--- check_update.py
+++ check_update.py
@@ -10,22 +10,27 @@ import os
 
 
 def cover_update():
-    repo = Repo(os.path.dirname(__file__))
-    g = repo.git
-    g.fetch('--all')
-    g.reset('--hard','main')
-    g.pull()
+    try:
+        repo = Repo(os.path.dirname(__file__))
+        repo.remote().pull()
+    except Exception as e:
+        print('发生异常: ',e,'\n尝试强制覆盖')
+        try:
+            g = repo.git
+            g.fetch('--all')
+            g.reset('--hard','main')
+            g.pull()
+        except Exception as e:
+            print('强制git发生异常',e)
+    finally:
+        repo.close()
 
 
 if __name__ == '__main__':
     while True:
         ans = input('是否进行github强制覆盖更新(Y/n)')
         if ans.__eq__('Y'):
-            try:
-                cover_update()
-                input('更新完毕')
-            except Exception as e:
-                input(e)
+            cover_update() 
             break
         elif ans.__eq__('n'):
             break
